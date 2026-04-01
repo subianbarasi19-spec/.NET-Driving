5a5a915398548a79b23400daec4d08609a06060f
Subi Devi <subianbarasi19@gmail.com>
2026-04-02 00:11:40 +0530

    Hello World

---
+++ HELLO WORLD/HELLO WORLD/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace HELLO_WORLD
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ demoproject/demoproject/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace demoproject
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
